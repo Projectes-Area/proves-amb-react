287d8b5e370829fa36b00372477fede12d0d34b9
Jaume Dellunde <jdellund@xtec.cat>
2019-03-28 07:53:03 +0100

    Updates

---
+++ precache-manifest.ba2b2c4214c2dcd4be6c92b7f3bb40dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa20cb792c95daf28808",
+    "url": "/react-fenologia/static/js/runtime~main.45c2217e.js"
+  },
+  {
+    "revision": "fa1d9f0839e07a5a228a",
+    "url": "/react-fenologia/static/js/main.786892ce.chunk.js"
+  },
+  {
+    "revision": "49350791c22969b20d26",
+    "url": "/react-fenologia/static/js/2.683f59cd.chunk.js"
+  },
+  {
+    "revision": "fa1d9f0839e07a5a228a",
+    "url": "/react-fenologia/static/css/main.5e963ff7.chunk.css"
+  },
+  {
+    "revision": "7a6da9fe94dd4993ae604ca37a8837a6",
+    "url": "/react-fenologia/index.html"
+  }
+];
